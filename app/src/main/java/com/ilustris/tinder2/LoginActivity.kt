340083c0f35fd02e206dd38e9c89da132c7e83f7
CaioProgramming <caioalvesferreiracontato@gmail.com>
2023-01-29 00:23:20 -0300

    First screen login

--- app/src/main/java/com/ilustris/tinder2/LoginActivity.kt
+++ app/src/main/java/com/ilustris/tinder2/LoginActivity.kt
@@ -2,8 +2,16 @@ package com.ilustris.tinder2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class Login : AppCompatActivity() {
+
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnLogin: Login: Button
+    private lateinit var edt: EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
