3e59e2eb56939be08c04054df8eccc7d87bb4115
DeboraFerreira <deboraprofil5@gmail.com>
2023-04-17 00:30:13 -0300

    implemented signup

--- app/src/main/java/com/ilustris/tinder2/LoginActivity.kt
+++ app/src/main/java/com/ilustris/tinder2/LoginActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
@@ -14,9 +15,12 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var edtPassword: EditText
     private lateinit var btnLogin: Button
     private lateinit var txtSignUp: TextView
+    private lateinit var mAuth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
 
         edtEmail = findViewById(R.id.edt_email)
         edtPassword = findViewById(R.id.edt_password)
@@ -27,6 +31,18 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
         }
+
+        btnLogin.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password= edtPassword.text.toString()
+
+            login(email,password)
+            
+        }
+
+    }
+
+    private fun login(email: String, password: String) {
 
     }
 }
--- app/src/main/java/com/ilustris/tinder2/SignupActivity.kt
+++ app/src/main/java/com/ilustris/tinder2/SignupActivity.kt
@@ -2,10 +2,56 @@ package com.ilustris.tinder2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
 
 class SignupActivity : AppCompatActivity() {
+
+    private lateinit var edtName: EditText
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnSignUp: Button
+    private lateinit var mAuth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        edtName = findViewById(R.id.edt_name)
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnSignUp = findViewById(R.id.btn_signup)
+
+
+
+        btnSignUp.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+            val name = edtName.text.toString()
+
+            signUp(email,password,name)
+        }
+    }
+
+    private fun signUp(email: String, password: String, name: String) {
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful){
+
+                    Log.d("SIGNUP", "success ")
+                    val user = mAuth.currentUser
+
+                } else{
+
+                    Log.e("SIGNUP", "ERROR" )
+                }
+
+            }
+
     }
 }
